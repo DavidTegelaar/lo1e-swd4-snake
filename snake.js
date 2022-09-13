175289b22f197f5b33ea214047ec1e6f5d643ad1
DavidTegelaar <6011247@mborijnland.nl>
2022-09-13 13:20:54 +0200

    interval game update

--- snake.js
+++ snake.js
@@ -4,5 +4,11 @@ const ctx = canvas.getContext("2d");
 ctx.fillStyle = "black";
 ctx.fillRect(0, 0, 400, 400);
 
-ctx.fillStyle = "green";
+ctx.fillStyle = "white";
 ctx.fillRect(190, 190, 20, 20);
+
+
+function update() {
+    console.log("Update");
+}
+setInterval(update, 1000);
