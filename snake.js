04d7b63d5bda17a8d449e98b1eb3bf1ca6445181
DavidTegelaar <74899056+DavidTegelaar@users.noreply.github.com>
2022-09-29 08:59:49 +0200

    snake move

--- snake.js
+++ snake.js
@@ -3,6 +3,15 @@ const ctx = canvas.getContext("2d");
 
 let snakeX = 200;
 let snakeY = 200;
+let snake = [{
+        x: 200,
+        y: 200
+    },
+    {
+        x: 220,
+        y: 200
+    }
+];
 
 let foodX;
 let foodY;
@@ -16,36 +25,47 @@ function drawBackground() {
 
 function drawSnake() {
     ctx.fillStyle = "white";
-    ctx.fillRect(snakeX, snakeY, 20, 20);
+    for (let index = 0; index < snake.length; index++) {
+        ctx.fillRect(snake[index].x, snake[index].y, 20, 20);
+    }
 }
 
 
 function update() {
+
+    for (let index = snake.length - 1; index > 0; index--) {
+
+    }
     if (direction == "right") {
-        if (snakeX < 380) {
-            snakeX += 20;
+        if (snake[0].x < 380) {
+            snake[0].x += 20;
         } else {
             gameOver();
         }
     } else if (direction == "left") {
-        if (snakeX > 0) {
-            snakeX -= 20;
+        if (snake[0].x > 0) {
+            snake[0].x -= 20;
         } else {
             gameOver();
         }
     } else if (direction == "up") {
-        if (snakeY > 0) {
-            snakeY -= 20;
+        if (snake[0].y > 0) {
+            snake[0].y -= 20;
         } else {
             gameOver();
         }
     } else if (direction == "down") {
-        if (snakeY < 380) {
-            snakeY += 20;
+        if (snake[0].y < 380) {
+            snake[0].y += 20;
         } else {
             gameOver();
         }
     }
+
+    if (foodX == snakeX && foodY == snakeY) {
+        spawnFood();
+    }
+
 
     drawBackground();
     drawSnake();
